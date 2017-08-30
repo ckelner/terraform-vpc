88f4495220965c84633b1c33a89085206ab4116e
Chris Kelner <ckelner@users.noreply.github.com>
2017-08-30 17:28:37 -0400

    Update aws-vpc.tf

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -15,6 +15,8 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
+  secret_key = "${var.aws_secret_key}"
+  token = "${var.aws_session_token}"
   region = "${var.aws_region}"
   skip_credentials_validation = true
 }
